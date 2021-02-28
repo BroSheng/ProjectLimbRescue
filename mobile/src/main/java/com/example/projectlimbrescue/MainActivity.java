1d98f7f019342d62dfdc2fa7f848fa049b7868d9
MonteCodes <michaelpmonte@outlook.com>
2021-02-28 12:40:13 -0500

    Code used for communication demo during Demo 1.

--- mobile/src/main/java/com/example/projectlimbrescue/MainActivity.java
+++ mobile/src/main/java/com/example/projectlimbrescue/MainActivity.java
@@ -1,29 +1,64 @@
 package com.example.projectlimbrescue;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.shared.Reading;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.gms.wearable.CapabilityClient;
 import com.google.android.gms.wearable.CapabilityInfo;
 import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.MessageClient;
 import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity implements DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener, CapabilityClient.OnCapabilityChangedListener {
 
     private static final String TAG = "MainActivity";
 
+    private Button mSendStartMessageBtn;
+    private TextView mTextView;
+
+    private static final String START_ACTIVITY_PATH = "/start-activity";
+
+    private LinkedList<Reading> readings = new LinkedList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupViews();
+
+
     }
 
     @Override
@@ -34,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements DataClient.OnData
         Wearable.getMessageClient(this).addListener(this);
         Wearable.getCapabilityClient(this)
                 .addListener(this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE);
-    }
+    }//.
 
     @Override
     protected void onPause() {
@@ -47,7 +82,33 @@ public class MainActivity extends AppCompatActivity implements DataClient.OnData
 
     @Override
     public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
-        // TODO: Transform the data into graphable data
+        Log.d(TAG, "onDataChanged(): " + dataEventBuffer);
+
+        for (DataEvent event : dataEventBuffer) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                String stop = "Start";
+                mSendStartMessageBtn.setText(stop);
+                DataItem dataItem = event.getDataItem();
+                Uri uri = dataItem.getUri();
+                String path = uri.getPath();
+                if (path.compareTo("/sensor") == 0) {
+                    DataMapItem item = DataMapItem.fromDataItem(dataItem);
+                    DataMap dm = item.getDataMap();
+                    long elapsedTime = dm.getLong("elapsedtime") / 1000;
+                    String readTime = "Took " + elapsedTime + " second reading";
+                    mTextView.setText(readTime);
+                }
+            }
+//            ByteArrayInputStream bis = new ByteArrayInputStream(event.getDataItem().getData());
+//            try {
+//                ObjectInput in = new ObjectInputStream(bis);
+//                readings = (LinkedList<Reading>) in.readObject();
+//                setText();
+//            } catch (IOException | ClassNotFoundException e) {
+//                e.printStackTrace();
+//            }
+
+        }
     }
 
     @Override
@@ -58,5 +119,89 @@ public class MainActivity extends AppCompatActivity implements DataClient.OnData
     @Override
     public void onMessageReceived(@NonNull MessageEvent messageEvent) {
         Log.d(TAG, "onMessageReceived: " + messageEvent);
+    }
+
+    public void onStartWearableActivityClick(View view) {
+        Log.d(TAG, "Generating RPC");
+
+        new StartWearableActivityTask().execute();
+        String stop = "Stop";
+        mSendStartMessageBtn.setText(stop);
+        mTextView.setText("");
+    }
+
+    @WorkerThread
+    private void sendStartActivityMessage(String node) {
+        Task<Integer> sendMessageTask = Wearable.getMessageClient(this).sendMessage(node, START_ACTIVITY_PATH, new byte[0]);
+
+        try {
+            // Block on a task and get the result synchronously (because this is on a background
+            // thread).
+            Integer result = Tasks.await(sendMessageTask);
+            Log.d(TAG, "Message sent: " + result);
+
+        } catch (ExecutionException exception) {
+            Log.e(TAG, "Task failed: " + exception);
+
+        } catch (InterruptedException exception) {
+            Log.e(TAG, "Interrupt occurred: " + exception);
+        }
+    }
+
+    @WorkerThread
+    private Collection<String> getNodes() {
+        HashSet<String> results = new HashSet<>();
+
+        Task<List<Node>> nodeListTask =
+                Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+
+        try {
+            // Block on a task and get the result synchronously (because this is on a background
+            // thread).
+            List<Node> nodes = Tasks.await(nodeListTask);
+
+            for (Node node : nodes) {
+                results.add(node.getId());
+            }
+
+        } catch (ExecutionException exception) {
+            Log.e(TAG, "Task failed: " + exception);
+
+        } catch (InterruptedException exception) {
+            Log.e(TAG, "Interrupt occurred: " + exception);
+        }
+
+        return results;
+    }
+
+    private void setText() {
+        if (readings == null) {
+            Log.e(TAG, "No readings available");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Reading reading : readings) {
+            String r = reading.channel0 + " " + reading.channel1 + " " + reading.time + "\n";
+            sb.append(r);
+        }
+        mTextView.setText(sb);
+    }
+
+    private void setupViews() {
+        mSendStartMessageBtn = (Button) findViewById(R.id.start_activity);
+        mTextView = (TextView) findViewById(R.id.textView);
+    }
+
+    private class StartWearableActivityTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... args) {
+            Collection<String> nodes = getNodes();
+            for (String node : nodes) {
+                sendStartActivityMessage(node);
+            }
+            return null;
+        }
     }
 }
--- wear/src/main/java/com/example/projectlimbrescue/MainActivity.java
+++ wear/src/main/java/com/example/projectlimbrescue/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.projectlimbrescue;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.example.myapplication.R;
+import com.example.shared.Reading;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.wearable.CapabilityClient;
@@ -22,7 +28,12 @@ import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 
@@ -30,18 +41,37 @@ import java.util.concurrent.ScheduledFuture;
  * TODO: Wearable activity is deprecated. We can transition away from it, but it will take some
  *  work.
  */
-public class MainActivity extends WearableActivity implements DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener, CapabilityClient.OnCapabilityChangedListener {
+public class MainActivity extends WearableActivity implements DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener, CapabilityClient.OnCapabilityChangedListener, SensorEventListener {
     private static final String TAG = "MainActivity";
 
+    private static final String START_ACTIVITY_PATH = "/start-activity";
     private static final String SENSOR_PATH = "/sensor";
     private static final String PPG_KEY = "ppg";
+
+    private boolean isLogging = false;
+    private SensorManager mSensorManager;
+    private int ppgSensor = 0;
+    private LinkedList<Reading> readingQueue = new LinkedList<>();
+    private long startTime;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView mTextView = (TextView) findViewById(R.id.text);
+        text = findViewById(R.id.text);
+//        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//
+//        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+//        for (Sensor sensor : sensorList) {
+//            Log.d("List sensors", "Name: ${currentSensor.name} /Type_String: ${currentSensor.stringType} /Type_number: ${currentSensor.type}");
+//            if(sensor.getStringType() == "com.google.wear.sensor.ppg")
+//            {
+//                ppgSensor = sensor.getType();
+//                Log.d("Sensor", "Using of type ${currentSensor.type}");
+//                break;
+//            }
+//        }
     }
 
     @Override
@@ -81,6 +111,55 @@ public class MainActivity extends WearableActivity implements DataClient.OnDataC
         + messageEvent.getRequestId()
         + " "
         + messageEvent.getPath());
+
+        if (messageEvent.getPath().equals(START_ACTIVITY_PATH)) {
+            if(isLogging) {
+                String x = "Done";
+                text.setText(x);
+                sendMockData(System.currentTimeMillis() - startTime);
+//                mSensorManager.unregisterListener(this);
+//                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//                ObjectOutputStream oos = null;
+//                try {
+//                    oos = new ObjectOutputStream(bos);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    oos.writeObject(readingQueue);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                byte[] serializedSensorData = bos.toByteArray();
+//                sendSensorData(serializedSensorData);
+//                readingQueue.clear();
+            } else {
+                String t = "Taking Reading";
+                text.setText(t);
+                startTime = System.currentTimeMillis();
+//                mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(ppgSensor), SensorManager.SENSOR_DELAY_FASTEST);
+            }
+
+            isLogging = !isLogging;
+        }
+    }
+
+    private void sendMockData(long data) {
+        PutDataMapRequest dataMap = PutDataMapRequest.create(SENSOR_PATH);
+        dataMap.getDataMap().putLong("elapsedtime", data);
+        dataMap.getDataMap().putLong("time", new Date().getTime());
+
+        PutDataRequest putDataRequest = dataMap.asPutDataRequest();
+        putDataRequest.setUrgent();
+
+        Task<DataItem> dataItemTask = Wearable.getDataClient(this).putDataItem(putDataRequest);
+        dataItemTask.addOnSuccessListener(
+                new OnSuccessListener<DataItem>() {
+                    @Override
+                    public void onSuccess(DataItem dataItem) {
+                        Log.d(TAG, "Sending data was successful: " + dataItem);
+                    }
+                });
     }
 
     private void sendSensorData(byte[] serializedSensorData) {
@@ -99,5 +178,17 @@ public class MainActivity extends WearableActivity implements DataClient.OnDataC
                         Log.d(TAG, "Sending data was successful: " + dataItem);
                     }
                 });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == ppgSensor) {
+            readingQueue.add(new Reading(event.timestamp, event.values[0], event.values[1]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }

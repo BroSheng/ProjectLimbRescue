23e208fa718251d6b01b3513220f48d20cd217e7
Sheng Yao <yao.sheng@outlook.com>
2021-12-11 20:54:23 -0500

    Update MainActivity.java

--- wear/src/main/java/com/example/projectlimbrescue/MainActivity.java
+++ wear/src/main/java/com/example/projectlimbrescue/MainActivity.java
@@ -143,11 +143,13 @@ public class MainActivity extends FragmentActivity implements
     private Date startDateTime, endDateTime;
 
     /**
-     * Server API address
+     * Server API address.
+     *
+     * Update the backend address here.
      */
-    private static final String readingTablePostingAddress = "http://18.212.219.27:8080/table";
-    private static final String readingDataPostingAddress = "http://18.212.219.27:8080/data";
-    private static final String timeAddress = "http://18.212.219.27:8080/time";
+    private static final String readingTablePostingAddress = "http://127.0.0.1:8080/table";
+    private static final String readingDataPostingAddress = "http://127.0.0.1:8080/data";
+    private static final String timeAddress = "http://127.0.0.1:8080/time";
 
 
 
